65551189860cc0a7f017ce263f05c38b784fc73c
Kevin Mulvey <kmulvey@linux.com>
2015-01-27 18:56:14 -0500

    formatting

--- server.go
+++ server.go
@@ -3,13 +3,18 @@ package main
 import (
 	"io"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
-
 func main() {
-	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, r.RemoteAddr)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		regex, _ := regexp.Compile(":[0-9]+$")
+		addr := regex.ReplaceAllString(r.RemoteAddr, "")
+		addr = strings.Replace(addr, "[", "", -1)
+		addr = strings.Replace(addr, "]", "", -1)
+		io.WriteString(w, addr)
 	})
-	http.ListenAndServe(":8000", nil)
-	http.ListenAndServe("[::]8000", nil)
+
+	http.ListenAndServe("[::]:8000", nil)
 }
